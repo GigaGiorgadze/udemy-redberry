f788581cde6bbe43a013aef509e7a96c46bf062a
Giga <giga.giorgadze.11@gmail.com>
2022-01-06 11:13:24 +0400

    feat add styling for meals Summary

---
+++ food-order/src/components/Meals/Meals.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AvalableMeals from "./AvalableMeals";
+import MealsSummary from "./MealsSumarry";
+
+function Meals() {
+	return (
+		<>
+			<MealsSummary />
+			<AvalableMeals />
+		</>
+	);
+}
+
+export default Meals;
