22a536c2eec4db201a670d7f8c8a4f8e550e16e8
Giga <giga.giorgadze.11@gmail.com>
2022-01-06 10:56:09 +0400

    feat: add Header card button

--- food-order/src/components/Layout/Header.jsx
+++ food-order/src/components/Layout/Header.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import MealImg from "../../assets/meals.jpg";
 
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 function Header(props) {
 	return (
 		<>
 			<header className={classes.header}>
 				<h1>ReactMeals</h1>
-				<button>Cart</button>
+				<HeaderCartButton />
 			</header>
 			<div className={classes["main-image"]}>
 				<img src={MealImg} alt="A table full of food!" />
