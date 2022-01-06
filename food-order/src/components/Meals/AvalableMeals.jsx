0adfdc8112842a469b6e9d051f7acf5239adcd74
Giga <giga.giorgadze.11@gmail.com>
2022-01-06 11:25:50 +0400

    feat: add better styling for avalable meals

--- food-order/src/components/Meals/AvalableMeals.jsx
+++ food-order/src/components/Meals/AvalableMeals.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import Card from "../UI/Card";
 import classes from "./AvalableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
 	{
@@ -30,12 +32,21 @@ const DUMMY_MEALS = [
 
 function AvalableMeals() {
 	const mealsList = DUMMY_MEALS.map((meal) => {
-		return <li key={meal.id}>{meal.name}</li>;
+		return (
+			<MealItem
+				key={meal.id}
+				name={meal.name}
+				description={meal.description}
+				price={meal.price}
+			/>
+		);
 	});
 
 	return (
 		<section className={classes.meals}>
-			<ul>{mealsList}</ul>
+			<Card>
+				<ul>{mealsList}</ul>
+			</Card>
 		</section>
 	);
 }
