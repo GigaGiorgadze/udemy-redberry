cf9b4c3de9ae86d15d444bb08e6d7d5ef20cdadf
Giga <giga.giorgadze.11@gmail.com>
2022-01-06 23:39:00 +0400

    feat: show price and items in card modal
    also users have ability to add new meal from modal if they want

--- food-order/src/components/Cart/Cart.jsx
+++ food-order/src/components/Cart/Cart.jsx
@@ -1,29 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 function Cart(props) {
-	const cartItems = (
-		<ul className={classes["cart-items"]}>
-			{[
-				{
-					id: "c1",
-					name: "Sushi",
-					amount: 2,
-					price: 12.99,
-				},
-			].map((item) => (
-				<li>{item.name}</li>
-			))}
-		</ul>
-	);
+	const ctx = useContext(CartContext);
+
+	const totalAmount = `$${ctx.totalAmount.toFixed(2)}`;
+	const hasItems = ctx.items.length > 0;
+
+	const cartItemRemoveHandler = (id) => {};
+
+	const cartItemAddHandler = (item) => {
+		ctx.addItem({ ...item, amount: 1 });
+	};
 
 	return (
 		<Modal hideCartHandler={props.hideCartHandler}>
-			{cartItems}
+			<ul className={classes["cart-items"]}>
+				{ctx.items.map((item) => {
+					return (
+						<CartItem
+							amount={item.amount}
+							key={item.id}
+							name={item.name}
+							price={item.price}
+							onRemove={cartItemRemoveHandler.bind(null, item.id)}
+							onAdd={cartItemAddHandler.bind(null, item)}
+						/>
+					);
+				})}
+			</ul>
 			<div className={classes.total}>
 				<span>Total amount</span>
-				<span>35.62</span>
+				<span>{totalAmount}</span>
 			</div>
 			<div className={classes.actions}>
 				<button
@@ -32,7 +43,7 @@ function Cart(props) {
 				>
 					Close
 				</button>
-				<button className={classes.button}>Order</button>
+				{hasItems && <button className={classes.button}>Order</button>}
 			</div>
 		</Modal>
 	);
