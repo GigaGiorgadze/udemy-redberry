61377d7005d6785e23fe603bc253d18e18513d3f
Giga <giga.giorgadze.11@gmail.com>
2022-01-08 23:32:35 +0400

    feat: user can order in database

--- food-order/src/components/Cart/Cart.js
+++ food-order/src/components/Cart/Cart.js
@@ -1,54 +1,91 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
-import Modal from '../UI/Modal';
-import CartItem from './CartItem';
-import classes from './Cart.module.css';
-import CartContext from '../../store/cart-context';
+import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
-  const cartCtx = useContext(CartContext);
+	const cartCtx = useContext(CartContext);
 
-  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
-  const hasItems = cartCtx.items.length > 0;
+	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+	const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {
-    cartCtx.removeItem(id);
-  };
+	const cartItemRemoveHandler = (id) => {
+		cartCtx.removeItem(id);
+	};
 
-  const cartItemAddHandler = (item) => {
-    cartCtx.addItem(item);
-  };
+	const cartItemAddHandler = (item) => {
+		cartCtx.addItem(item);
+	};
 
-  const cartItems = (
-    <ul className={classes['cart-items']}>
-      {cartCtx.items.map((item) => (
-        <CartItem
-          key={item.id}
-          name={item.name}
-          amount={item.amount}
-          price={item.price}
-          onRemove={cartItemRemoveHandler.bind(null, item.id)}
-          onAdd={cartItemAddHandler.bind(null, item)}
-        />
-      ))}
-    </ul>
-  );
+	const cartItems = (
+		<ul className={classes["cart-items"]}>
+			{cartCtx.items.map((item) => (
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+					onRemove={cartItemRemoveHandler.bind(null, item.id)}
+					onAdd={cartItemAddHandler.bind(null, item)}
+				/>
+			))}
+		</ul>
+	);
+var items = cartCtx.items
 
-  return (
-    <Modal onClose={props.onClose}>
-      {cartItems}
-      <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>{totalAmount}</span>
-      </div>
-      <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>
-          Close
-        </button>
-        {hasItems && <button className={classes.button}>Order</button>}
-      </div>
-    </Modal>
-  );
+	async function orderHandler() {
+		try {
+			const response = await fetch(
+				"https://react-http-9c241-default-rtdb.europe-west1.firebasedatabase.app/meals.json", {
+          method: "post",
+          body: JSON.stringify(cartCtx.items),
+          headers: {
+            "Content-Type" : "application/json"
+          }
+        }
+			);
+
+			// const data = await response.json();
+
+			// const loadedMeals = [];
+
+			// for (const key in data) {
+			// 	loadedMeals.push({
+			// 		id: key,
+			// 		name: data[key].name,
+			// 		description: data[key].description,
+			// 		price: data[key].price,
+			// 	});
+			// }
+			// console.log(data);
+
+			// setMeals(loadedMeals);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	return (
+		<Modal onClose={props.onClose}>
+			{cartItems}
+			<div className={classes.total}>
+				<span>Total Amount</span>
+				<span>{totalAmount}</span>
+			</div>
+			<div className={classes.actions}>
+				<button className={classes["button--alt"]} onClick={props.onClose}>
+					Close
+				</button>
+				{hasItems && (
+					<button onClick={orderHandler} className={classes.button}>
+						Order
+					</button>
+				)}
+			</div>
+		</Modal>
+	);
 };
 
 export default Cart;
--- food-order/src/components/Meals/MealItem/MealItem.js
+++ food-order/src/components/Meals/MealItem/MealItem.js
@@ -16,6 +16,7 @@ const MealItem = (props) => {
       amount: amount,
       price: props.price
     });
+
   };
 
 
