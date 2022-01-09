bb09e3b5c57a0dca522ece1b5961ede6b21e5406
Giga <giga.giorgadze.11@gmail.com>
2022-01-09 18:54:26 +0400

    inital commit for redux cart

--- redux-with-react/src/components/Header.js
+++ redux-with-react/src/components/Header.js
@@ -14,15 +14,19 @@ const Header = () => {
 		<header className={classes.header}>
 			<h1>Redux Auth</h1>
 			<nav>
-				<ul>
-					<li>
-						<a href="/">My Products</a>
-					</li>
-					<li>
-						<a href="/">My Sales</a>
-					</li>
-					<li>{isAuth && <button onClick={logoutHandler}>Logout</button>}</li>
-				</ul>
+				{isAuth && (
+					<ul>
+						<li>
+							<a href="/">My Products</a>
+						</li>
+						<li>
+							<a href="/">My Sales</a>
+						</li>
+						<li>
+							<button onClick={logoutHandler}>Logout</button>
+						</li>
+					</ul>
+				)}
 			</nav>
 		</header>
 	);
