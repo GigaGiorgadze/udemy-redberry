2fd52700930af4190a0007f0dcc6fbecff8273eb
Giga <giga.giorgadze.11@gmail.com>
2022-01-09 13:00:26 +0400

    feat: add toggle button and increment by 5 btn

--- redux-with-react/src/components/Counter.js
+++ redux-with-react/src/components/Counter.js
@@ -4,6 +4,7 @@ import classes from "./Counter.module.css";
 const Counter = () => {
 	const dispatch = useDispatch();
 	const counter = useSelector((state) => state.counter);
+	const showCounter = useSelector((state) => state.showCounter);
 
 	const incrementHandler = () => {
 		dispatch({ type: "increment" });
@@ -13,16 +14,23 @@ const Counter = () => {
 		dispatch({ type: "decrement" });
 	};
 
-	const toggleCounterHandler = () => {};
+	const incrementByFiveHandler = () => {
+		dispatch({ type: "increase", value: 5 });
+	};
+
+	const toggleCounterHandler = () => {
+		dispatch({ type: "toggle" });
+	};
 
 	return (
 		<main className={classes.counter}>
 			<h1>Redux Counter</h1>
 			<div>
+				<button onClick={incrementByFiveHandler}>increment by five</button>
 				<button onClick={incrementHandler}>increment</button>
 				<button onClick={decrementHandler}>decrement</button>
 			</div>
-			<div className={classes.value}>{counter}</div>
+			{showCounter && <div className={classes.value}>{counter}</div>}
 			<button onClick={toggleCounterHandler}>Toggle Counter</button>
 		</main>
 	);
