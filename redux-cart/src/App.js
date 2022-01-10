908c34c69ef999c8f1c1a531e7a4c5192fa4a904
Giga <giga.giorgadze.11@gmail.com>
2022-01-10 10:40:34 +0400

    feat: show notifications for adding item to cart

--- redux-cart/src/App.js
+++ redux-cart/src/App.js
@@ -1,28 +1,42 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { sendCartdata } from "./store/card-slice";
+
+let isInitial = true;
 
 function App() {
 	const showCart = useSelector((state) => state.ui.cartIsVisible);
 	const cart = useSelector((state) => state.cart);
+	const dispatch = useDispatch();
+	const notification = useSelector((state) => state.ui.notifications);
 
 	useEffect(() => {
-		fetch(
-			"https://react-http-9c241-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-			{
-				method: "PUT",
-				body: JSON.stringify(cart),
-			}
-		);
-	}, [cart]);
+		if (isInitial) {
+			isInitial = false;
+			return;
+		}
+
+		dispatch(sendCartdata(cart));
+	}, [cart, dispatch]);
 
 	return (
-		<Layout>
-			{showCart && <Cart />}
-			<Products />
-		</Layout>
+		<>
+			{notification && (
+				<Notification
+					status={notification.status}
+					title={notification.title}
+					message={notification.message}
+				/>
+			)}
+			<Layout>
+				{showCart && <Cart />}
+				<Products />
+			</Layout>
+		</>
 	);
 }
 
