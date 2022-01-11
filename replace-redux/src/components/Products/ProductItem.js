4de1e4649e8c5bf05f91d77b64cee162f2f3a3c0
Giga <giga.giorgadze.11@gmail.com>
2022-01-11 22:19:17 +0400

    refactor: now add ability to add products to favorite using use context

--- replace-redux/src/components/Products/ProductItem.js
+++ replace-redux/src/components/Products/ProductItem.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { ProductsContext } from "../../context/products-contentx";
 
-import Card from '../UI/Card';
-import './ProductItem.css';
+import Card from "../UI/Card";
+import "./ProductItem.css";
 
-const ProductItem = props => {
+const ProductItem = (props) => {
+	const toggleFav = useContext(ProductsContext).toggleFav;
 
-  const toggleFavHandler = () => {
-    
-  };
+	const toggleFavHandler = () => {
+		toggleFav(props.id);
+	};
 
-  return (
-    <Card style={{ marginBottom: '1rem' }}>
-      <div className="product-item">
-        <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
-        <p>{props.description}</p>
-        <button
-          className={!props.isFav ? 'button-outline' : ''}
-          onClick={toggleFavHandler}
-        >
-          {props.isFav ? 'Un-Favorite' : 'Favorite'}
-        </button>
-      </div>
-    </Card>
-  );
+	return (
+		<Card style={{ marginBottom: "1rem" }}>
+			<div className="product-item">
+				<h2 className={props.isFav ? "is-fav" : ""}>{props.title}</h2>
+				<p>{props.description}</p>
+				<button
+					className={!props.isFav ? "button-outline" : ""}
+					onClick={toggleFavHandler}
+				>
+					{props.isFav ? "Un-Favorite" : "Favorite"}
+				</button>
+			</div>
+		</Card>
+	);
 };
 
 export default ProductItem;
