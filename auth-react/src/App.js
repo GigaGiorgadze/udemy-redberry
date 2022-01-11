66dd3754970e9cf19f592c9d419b00a1f681feb9
Giga <giga.giorgadze.11@gmail.com>
2022-01-11 10:54:11 +0400

    feat: protect routes

--- auth-react/src/App.js
+++ auth-react/src/App.js
@@ -1,26 +1,37 @@
-import { Switch, Route } from 'react-router-dom';
+import { useContext } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import AuthContext from "./store/auth-contenxt";
 
 function App() {
-  return (
-    <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <HomePage />
+	const authCtx = useContext(AuthContext);
+
+	return (
+		<Layout>
+			<Switch>
+				<Route path="/" exact>
+					<HomePage />
+				</Route>
+				{!authCtx.isLoggedIn && (
+					<Route path="/auth">
+						<AuthPage />
+					</Route>
+				)}
+				{authCtx.isLoggedIn && (
+					<Route path="/profile">
+						<UserProfile />
+					</Route>
+				)}
+        <Route path='*'>
+          <Redirect to='/' />
         </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
-        </Route>
-      </Switch>
-    </Layout>
-  );
+			</Switch>
+		</Layout>
+	);
 }
 
 export default App;
