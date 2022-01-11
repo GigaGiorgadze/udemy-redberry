648c35f5334f6dc75d98f37ec8ab44a5d483fe04
Giga <giga.giorgadze.11@gmail.com>
2022-01-11 22:10:32 +0400

    refacotor: load products with useContext instead of redux

--- replace-redux/src/index.js
+++ replace-redux/src/index.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import './index.css';
-import App from './App';
-import productReducer from './store/reducers/products';
+import "./index.css";
+import App from "./App";
+import ProductsProducer from "./context/products-contentx";
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
-
-const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+	<ProductsProducer>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</ProductsProducer>,
+	document.getElementById("root")
 );
