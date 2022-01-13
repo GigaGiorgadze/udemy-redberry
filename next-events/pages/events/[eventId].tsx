024210d2a73cd17c6bd40fa86d5716a421aa9e71
Giga <giga.giorgadze.11@gmail.com>
2022-01-13 20:23:37 +0400

    feat: add and style single event page

--- next-events/pages/events/[eventId].tsx
+++ next-events/pages/events/[eventId].tsx
@@ -1,5 +1,32 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/events/event-detail/event-summary";
+import EventLogistics from "../../components/events/event-detail/event-logistics";
+import EventContent from "../../components/events/event-detail/event-content";
+
 const EventDetailPage = () => {
-    return <div>SINGE EventDetailPage page</div>
+	const router = useRouter();
+	const { eventId } = router.query;
+	const event = getEventById(eventId);
+
+	if (!event) {
+		return <p>No event found!</p>;
+	}
+
+	return (
+		<>
+			<EventSummary title={event.title} />
+			<EventLogistics
+				date={event.date}
+				address={event.location}
+				image={event.image}
+				imageAlt={event.title}
+			/>
+			<EventContent>
+				<p>{event.description}</p>
+			</EventContent>
+		</>
+	);
 };
 
 export default EventDetailPage;
