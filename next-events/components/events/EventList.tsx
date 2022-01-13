78b0dcf066e357381a705e9ed61ad7ce8ab0b49f
Giga <giga.giorgadze.11@gmail.com>
2022-01-13 17:47:47 +0400

    feat; add icons

--- next-events/components/events/EventList.tsx
+++ next-events/components/events/EventList.tsx
@@ -1,7 +1,9 @@
-import Link from "next/link";
 import React from "react";
 import Button from "../UI/Button";
 import classes from "./EventItem.module.css";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 const EventList: React.FC<{
 	items: {
@@ -24,7 +26,8 @@ const EventList: React.FC<{
 					<div className={classes.content}>
 						<div className={classes.summery}>
 							<h2>{item.title}</h2>
-							<div className={classes.data}>
+							<div className={classes.date}>
+								<DateIcon />
 								<time>
 									{new Date(item.date).toLocaleDateString("en-US", {
 										day: "numeric",
@@ -34,11 +37,17 @@ const EventList: React.FC<{
 								</time>
 							</div>
 							<div className={classes.address}>
+								<AddressIcon />
 								<address>{item.location.replace(", ", "\n")}</address>
 							</div>
 						</div>
 						<div className={classes.actions}>
-							<Button link={`/events/${item.id}`}>Explore event</Button>
+							<Button link={`/events/${item.id}`}>
+								<span>Explore event</span>
+								<span className={classes.icon}>
+									<ArrowRightIcon />
+								</span>
+							</Button>
 						</div>
 					</div>
 				</li>
